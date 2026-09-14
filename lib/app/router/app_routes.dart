d781b5e41f197272353cfca6d700153ef6f6723f
joud <joudysami13@gmail.com>
2026-09-15 00:23:50 +0300

    feat: Add core

---
+++ lib/app/router/app_routes.dart
@@ -0,0 +1,14 @@
+final class AppRoutes {
+  static const String onBoarding = '/onboarding';
+  static const String login = '/login';
+  static const String apply = '/apply';
+  static const String successApply = '/successApply';
+  static const String forgetPassword = '/forgetPassword';
+  static const String verification = '/verification';
+  static const String resetPassword = '/resetPassword';
+  static const String profile = '/profile';
+  static const String editVehicle = '/editVehicle';
+
+  static const String editProfile = '/editProfile';
+  static const String home = '/home';
+}
---
+++ lib/core/network/safe_call.dart
@@ -0,0 +1,19 @@
+import 'package:injectable/injectable.dart';
+import 'package:tracking_app/core/base/base_response.dart';
+import 'package:tracking_app/core/error/error_parser.dart';
+@injectable
+class SafeCall {
+  Future<BaseResponse<T>> safeApiCall<T>(Future<T> Function() apiCall) async {
+    try {
+      final response = await apiCall();
+      return SuccessResponse(response);
+    } catch (e) {
+      final appError = errorParser(
+        e is Exception ? e : Exception(e.toString()),
+      );
+      return ErrorResponse(appError: appError);
+    }
+  }
+}
+//try catch//
+//return safeApiCall(() => api.login());
